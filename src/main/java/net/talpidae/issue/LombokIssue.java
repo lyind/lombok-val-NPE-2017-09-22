00753a379d44bf07edf0420a8cb1110812d08605
Jonas Zeiger <jonas.zeiger@talpidae.net>
2017-09-22 10:30:45 +0200

    Initial commit.

---
+++ src/main/java/net/talpidae/issue/LombokIssue.java
@@ -0,0 +1,35 @@
+package net.talpidae.issue;
+
+import lombok.val;
+
+import java.util.stream.Stream;
+
+
+/**
+ * Simple test case for Lombok NPE at build time.
+ */
+public class LombokIssue extends Thread
+{
+    private LombokIssue()
+    {
+        super(() ->
+        {
+            for (int i = 0; i < 5; ++i)
+            {
+                val index = i;
+                Stream.of(new Character[]{'a', '1', 'b', '2', '3', 'c', 'd', '4', 'e', '5', 'f', 'g'})
+                        .filter(Character::isDigit)
+                        .forEach(character ->
+                        {
+                            System.out.println(Integer.toString(index) + ": " + character);
+                        });
+            }
+        });
+    }
+
+
+    void main(String[] args)
+    {
+        new LombokIssue().run();
+    }
+}
